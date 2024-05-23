dabab4b8ad6c09ace6115ec84cb9cfd9e68eea72
Chris Andreae <chris@andreae.gen.nz>
2024-05-23 11:55:28 +0900

    Compiler service: pass keymap data when building RHS

--- lambda/compiler.rb
+++ lambda/compiler.rb
@@ -23,8 +23,8 @@ class Compiler
     board_rh = board + '_rh'
 
     if rhs_kconfig_data || !snippets.empty?
-      lhs_result, lhs_output = compile_board(board_lh, keymap_data:,     kconfig_data: lhs_kconfig_data, snippets:, include_static_rhs: false)
-      rhs_result, rhs_output = compile_board(board_rh, keymap_data: nil, kconfig_data: rhs_kconfig_data, snippets:, include_static_rhs: false)
+      lhs_result, lhs_output = compile_board(board_lh, keymap_data:, kconfig_data: lhs_kconfig_data, snippets:, include_static_rhs: false)
+      rhs_result, rhs_output = compile_board(board_rh, keymap_data:, kconfig_data: rhs_kconfig_data, snippets:, include_static_rhs: false)
       [
         lhs_result.concat(rhs_result),
         ["LHS Output:", *lhs_output, "RHS Output:", *rhs_output],
